ddf8e05187240e4a938da4ff73f30d9f49f0235e
SagarGYadav <118979610+SagarGYadav@users.noreply.github.com>
2023-01-17 11:11:44 +0530

    Validating on every keyStroke

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -14,6 +14,10 @@ const SimpleInput = (props) => {
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
+
+    if (e.target.value.trim() !== "") {
+      setEnteredNameIsValid(true);
+    }
   };
 
   const nameInputBlurHandler = (e) => {
@@ -21,7 +25,6 @@ const SimpleInput = (props) => {
 
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
-      return;
     }
   };
 
